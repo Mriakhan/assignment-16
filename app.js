0e577aa875d9272792defc86e08a9f6f56523d65
Mriakhan <mmk762126@gmail.com>
2024-02-22 14:28:03 +0500

    add assignment 16

---
+++ app.js
@@ -0,0 +1,129 @@
+// chapter1
+// Q1
+// alert("hello! welcome to java script")
+// Q2
+// alert("Error! please enter a correct password")
+// Q3
+// alert("Welcome to JS land.... \n Happy coding. ")
+// Q4
+// alert("welcome to JS land")
+// Q5
+// alert("Hello! i can run JS through")
+
+
+                                //    ALERT
+    // Q1  
+    // alert("Maria")     
+    // alert("khan")
+    // alert("mria.khn@gmail.com")
+    // alert("0311828222356")
+    // alert("*********")
+    // Q2// alert("you are learning java script")
+    // Q3
+    // alert("hey! javascript world")
+
+    // CHAPTER 2
+
+    // Q1
+    // var a =("maria khan");
+    // console.log(a)
+
+    // var a =
+    // a = "my name is MARIA";
+    // console.log(a);
+
+// var team =("PAKISTAN");
+// console.log(team)
+// alert("pakistan is my team")
+// var bestman ="maria";
+// console.log(bestman)
+
+// CHAPTER 3
+// var number = 30;
+// console.log(number)
+
+// var value =144;
+// console.log(value)
+
+// var num= 9;
+// console.log(num)
+
+// var a = 6 ;
+// var b = 2 ;
+// console.log(a,b)
+
+// var merchtotal = 100;
+// var shippingcharge = 10;
+// var totalorder = merchtotal + shippingcharge ;
+// console.log(merchtotal+shippingcharge);
+// document.write(totalorder)
+
+// var a = 100 ;
+// var b = 40 ;
+// console.log(a+b)
+
+// VARIABLE FOR NUMBERS
+
+// var age = 15 ;
+// alert("i'm" + age + "years old" )
+
+// var num = 15 ;
+// alert("You have visited this" + num + "times" )
+
+// var birthyear = 1994;
+// document.write("my birth is " , birthyear ,  " <br/> data type of my declared variable is number")
+
+// var visitorsname = "john doe"; 
+// var producttitle = "online order";
+// var quantity ="5-6 t-shirts";
+// document.write(  visitorsname ,  producttitle , quantity , "on www.XYZclothing.com");
+
+// VARIABLR FOR STRING
+
+// var username = "@Maria Khan"
+// document.write(username)
+
+// var myname = "MARIA KHAN" ;
+// alert(myname);
+
+// var a = "Hello World";
+// alert(a);
+
+// var a = "John Doe";
+// alert(a);
+
+// var a = "15 years old";
+// alert(a);
+
+// var a = "certified mobile application developer";
+// alert(a);
+
+// var a = "PIZZA";
+// var b = "PIZZ";
+// var c = "PIZ";
+// var d = "PI";
+// var e = "P";
+
+// alert( a + "\n" + b + "\n" + c  + "\n" + d + "\n" + e   );
+
+// var email = "mria.khan1234@gmail.com";
+// alert("my email address is" + email );
+
+// var a = "A smarter way to learn Java Script"
+// alert( "iam trying to learn from the book" + a );
+
+var a = "-------------";
+var b = "😀👩‍💻😀"
+var c = "-------------";
+alert(a + b + c );
+document.write(a + b + c);
+
+
+
+
+
+
+
+
+    
+
